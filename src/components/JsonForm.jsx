de6f9235adbecffa35f45c0da4ea57bc4f57558b
Carlos Lucientes <carloslucientes@oboria.com>
2024-09-26 14:25:18 +0200

    Form y powerAutomate

--- src/components/JsonForm.jsx
+++ src/components/JsonForm.jsx
@@ -13,70 +13,74 @@ const initialData = {
   name: "Mr/Mrs Oboria",
   responsable: true,
   irresponsable: false,
-  fecha:new Date().toISOString().split('T')[0],
+  fecha: new Date().toISOString().split("T")[0],
 };
-
 
 export const JsonFormsProb = () => {
   const [data, setData] = useState(initialData);
-  const jsonData =useMemo(()=>JSON.stringify(data, null, 2),[data]) ;
-  
-  
+  const jsonData = useMemo(() => JSON.stringify(data, null, 2), [data]);
+
   const handleSubmit = () => {
     // #BlobRegion
-    const dataBlob = new Blob([jsonData], { type: 'application/json' });
+    const dataBlob = new Blob([jsonData], { type: "application/json" });
     const url = URL.createObjectURL(dataBlob);
-    setData(url);  // Guardar la URL generada
-    console.log(url)
-    window.open(url, '_blank');
+    setData(url); // Guardar la URL generada
+    console.log(url);
+    window.open(url, "_blank");
     // #endRegion
-    const urlPower= 'https://prod-93.westeurope.logic.azure.com:443/workflows/82828cccadf346ef9f870a0cc40cca1f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AMDsphYZcS3quqkcVzaub3Y6hzJZylJumA4dfUHFVrA';
-    
-    axios.post (urlPower,jsonData )
-    .then(response => {
-      setData(response.jsonData);
-      console.log(response.jsonData);  // Ver la respuesta en consola
-  })
-  .catch(error => {
-      console.error('Error:', error);
-  });
-  return (
-    <div>
-        <button >Hacer POST</button>
-       {jsonData && <div>Respuesta: {JSON.stringify(jsonData)} </div>}
-    </div>
-    
-);
+    const urlPower =
+      "https://prod-93.westeurope.logic.azure.com:443/workflows/82828cccadf346ef9f870a0cc40cca1f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AMDsphYZcS3quqkcVzaub3Y6hzJZylJumA4dfUHFVrA";
 
+      // Añadimos el header para que llegue en formato Json en Power Autoamte
+    axios
+      .post(urlPower, jsonData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setData(response.jsonData);
+        console.log(response.jsonData); // Ver la respuesta en consola
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    return (
+      <div>
+        <button>Hacer POST</button>
+      </div>
+    );
   };
-
   return (
     <>
-    <div>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data || {}}
-        renderers={materialRenderers}
-        cells={materialCells}
-        onChange={({ data }) => setData(data)}
-      />
+      <div>
+        <JsonForms
+          schema={schema}
+          uischema={uischema}
+          data={data || {}}
+          renderers={materialRenderers}
+          cells={materialCells}
+          onChange={({ data }) => setData(data)}
+        />
       </div>
       <div>
-      <Button
-        onClick={handleSubmit}
-        color="primary"
-        variant="contained"  
-        sx={{ marginRight: 3 }}  >
-        Salvameeeeh!!
-      </Button>
-      {jsonData && <div>Respuesta: {JSON.stringify(jsonData)} </div>}
-
-      <Button onClick={() => setData({})} color="secondary" variant="contained">
-        Clear form data
-      </Button>
+        <Button
+          onClick={handleSubmit}
+          color="primary"
+          variant="contained"
+          sx={{ marginRight: 3 }}
+        >
+          Salvameeeeh!!
+        </Button>
+        <Button
+          onClick={() => setData({})}
+          color="secondary"
+          variant="contained"
+        >
+          Clear form data
+        </Button>
       </div>
-      </>
+    </>
   );
 };
 
@@ -86,4 +90,9 @@ export default JsonForms;
 const url = URL.createObjectURL(dataBlob);
 setJsonUrl(url);  // Guardar la URL generada
 console.log(url)
-window.open(url, '_blank'); */
+window.open(url, '_blank');
+
+Esto muestra la respuesta:
+ {jsonData && <div>Respuesta: {JSON.stringify(jsonData)} </div>}
+
+*/
