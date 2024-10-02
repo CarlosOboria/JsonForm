21e6333f579f6ab394debf165d3e066619bef71a
Carlos Lucientes <carloslucientes@oboria.com>
2024-10-02 14:09:56 +0200

    1º intento azure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css'
 import { JsonFormsProb }  from './components/JsonForm'
+import { InteractionType } from "@azure/msal-browser";
+import { MsalAuthenticationTemplate, useMsal } from "@azure/msal-react";
 
+function WelcomeUser() {
+  const { accounts } = useMsal();
+  const username = accounts[0].username;
+
+  return <p>Welcome, {username}</p>;
+}
 
 function App() {
-
-
-
-
   return (
-    <div>
-      <h1>Formulario JeiSON</h1>
-      <h2>para Oboria S.L.</h2>
-   
-     <JsonFormsProb/>
-    </div>
+    <MsalAuthenticationTemplate interactionType={InteractionType.Popup}>
+    <p>This will only render if a user is not signed-in.</p>
+    <WelcomeUser />
+  </MsalAuthenticationTemplate>
   )
 }
 
 export default App
+
+/* 
+<div>
+<h1>Formulario JeiSON</h1>
+<h3>para Oboria S.L.</h3>
+
+<JsonFormsProb/>
+</div> */
